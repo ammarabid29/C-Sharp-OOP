7ff92e78dc7327cf7d3615aff2a2643e9852919d
ammarabid29 <aammarawan786@gmail.com>
2025-03-12 14:00:48 +0500

    Basic Classes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace C_Sharp_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new()
+            {
+                name = "John",
+                age = "20",
+                marks = 90
+            };
+            student.detail();
+
+        }
+    }
+    class Student
+    {
+        public required string name;
+        public required string age;
+        public int marks;
+
+        public void detail()
+        {
+            Console.WriteLine($"Name: {name}, Age: {age}, Marks: {marks}");
+        }
+    }
+}
